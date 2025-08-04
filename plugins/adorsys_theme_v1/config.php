6c3250816c301cbed940d981d49880891ff52744
freshystar <thondipiongange@gmail.com>
2025-08-04 16:01:57 +0100

    Enhance Adorsys Theme with interactive light/dark mode toggle
    • Enabled 'class' based dark mode in Tailwind config\n• Included theme.js for toggle button in config.php\n• Added JavaScript to switch themes and persist preference

--- plugins/adorsys_theme_v1/config.php
+++ plugins/adorsys_theme_v1/config.php
@@ -4,6 +4,7 @@ defined('MOODLE_INTERNAL') || die();
 $THEME->name = 'adorsys_theme_v1';
 $THEME->parents = ['boost'];
 $THEME->sheets = ['all'];
+$THEME->javascripts = ['theme'];
 $THEME->editor_sheets = [];
 $THEME->enable_dock = false;
 $THEME->rendererfactory = 'theme_overridden_renderer_factory';
--- plugins/adorsys_theme_v1/tailwind.config.js
+++ plugins/adorsys_theme_v1/tailwind.config.js
@@ -1,10 +1,13 @@
 module.exports = {
+  darkMode: 'class',
+  darkMode: 'class',
   content: [
     './**/*.php',
     './layout/**/*.php',
     './templates/**/*.mustache',
     './scss/**/*.scss',
-    './src/**/*.{js,ts}'
+    './src/**/*.{js,ts}',
+    './javascript/**/*.js'
   ],
   theme: {
     extend: {},
