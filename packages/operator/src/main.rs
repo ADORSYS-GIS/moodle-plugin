b4db90519ffd57fec91c871b10fd13bc3481e341
Leghadjeu-Christian <legochristianarthur@gmail.com>
2025-08-12 16:38:14 +0100

    feat(telemetry): add OpenTelemetry logging and Prometheus metrics
    - Added `init_logs_and_tracing()` to initialize structured logging.
    - Integrated OpenTelemetry tracing via OTLP exporter.
    - Log level configured via `RUST_LOG`, and exporter endpoint via `OTEL_LOGS_EXPORTER_ENDPOINT`.
    - Added Prometheus metrics (`/metrics`) for HTTP requests, memory, and CPU usage.
    - Added validation test for Moodle CRD specs.
    - Removed module path from log output for cleaner logs.

--- packages/operator/src/main.rs
+++ packages/operator/src/main.rs
@@ -5,7 +5,8 @@ use mimalloc::MiMalloc;
 mod crds;
 mod error;
 mod reconciller;
-use crate::reconciller::controller::controller_moodle_cluster;
+mod telemetry;
+use crate::{reconciller::controller::controller_moodle_cluster, telemetry::{logging::init_logs_and_tracing,telemetry_server::{ start_otel_server}}};
 
 #[derive(Clone)]
 struct Data {
@@ -15,15 +16,22 @@ struct Data {
 #[global_allocator]
 static GLOBAL: MiMalloc = MiMalloc;
 
-
 #[tokio::main]
 async fn main() -> Result<()> {
-    tracing_subscriber::fmt::init();
+    // tracing_subscriber::fmt::init();
+    init_logs_and_tracing();
     
     let client = Client::try_default().await?;
     
-    info!("Started controller");
-    controller_moodle_cluster(&client).await;
+    let client_clone = client.clone();
+    info!("Started controller and server...");
+    
+    tokio::spawn(
+        async move {
+            controller_moodle_cluster(&client_clone).await;
+        }
+    );
+    let _ = start_otel_server().await;
     
     Ok(())
 }
