8c9ca0ab0356789083d06f1994737efce65a1659
Leghadjeu-Christian <legochristianarthur@gmail.com>
2025-08-14 10:42:59 +0100

    chore(dev): remove unnecessary development files

--- test-local-prometheus.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Run Prometheus locally on host port 9091 to avoid port 9090 conflict
-docker run --rm \
-  -p 9091:9090 \
-  -v "$(pwd)/prometheus-local.yml":/etc/prometheus/prometheus.yml \
-  prom/prometheus
-echo "Prometheus is available at http://localhost:9091"
