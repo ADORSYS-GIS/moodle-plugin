2f47d8389f4609977ab6a34b484f65d350cd3317
freshystar <thondipiongange@gmail.com>
2025-10-13 09:03:41 +0100

    refactor: updated version of the moodle Removed init script in rust-sidecar and README file

--- plugins/local/openai_assistant/version.php
+++ plugins/local/openai_assistant/version.php
@@ -1,7 +1,7 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2024091800; // YYYYMMDDHH
+$plugin->version   = 2025100900; // YYYYMMDDHH
 $plugin->requires  = 2022041900; // Moodle 4.0+
 $plugin->component = 'local_openai_assistant';
 $plugin->maturity  = MATURITY_STABLE;
