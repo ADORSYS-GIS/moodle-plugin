c84734d340b2340b7bf6df2577192be033c41754
prosper-ador <prosnyengka@gmail.com>
2025-10-20 07:57:10 +0100

    feat(provider-output): add plugin renderer

---
+++ plugins/ai/provider/gis_ai/classes/output/renderer.php
@@ -0,0 +1,11 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+
+declare(strict_types=1);
+
+namespace aiprovider_gis_ai\output;
+
+defined('MOODLE_INTERNAL') || die();
+
+class renderer extends \plugin_renderer_base {
+}
