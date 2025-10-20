ed76e80562b8d4224d1b6dbf88e2344b7220ad93
prosper-ador <prosnyengka@gmail.com>
2025-10-20 07:57:47 +0100

    feat(placement): add GIS AI Chat placement version.php

---
+++ plugins/ai/placement/gis_ai_chat/version.php
@@ -0,0 +1,10 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'aiplacement_gis_ai_chat';
+$plugin->version   = 2025102001; // YYYYMMDDXX.
+$plugin->requires  = 2024042200; // Moodle 4.5 or later.
+$plugin->maturity  = MATURITY_ALPHA;
+$plugin->release   = '0.1.0';
