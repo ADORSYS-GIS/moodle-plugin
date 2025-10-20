db8bca99cdb0a697c473c2087a1c273413b4e5b5
prosper-ador <prosnyengka@gmail.com>
2025-10-20 07:57:48 +0100

    feat(placement-db): register external function for chat send

---
+++ plugins/ai/placement/gis_ai_chat/db/services.php
@@ -0,0 +1,15 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+
+defined('MOODLE_INTERNAL') || die();
+
+$functions = [
+    'aiplacement_gis_ai_chat_send' => [
+        'classname'   => '\\aiplacement_gis_ai_chat\\external\\send',
+        'methodname'  => 'execute',
+        'description' => 'Send prompt to AI via core_ai manager and return response',
+        'type'        => 'write',
+        'capabilities'=> 'aiplacement/gis_ai_chat:generate_text',
+        'ajax'        => true,
+    ],
+];
