1f4e9d3c1d3a493dc3d5ecdce9f323682bda11b6
prosper-ador <prosnyengka@gmail.com>
2025-10-20 07:57:02 +0100

    chore(provider-events): add observers placeholder

---
+++ plugins/ai/provider/gis_ai/db/events.php
@@ -0,0 +1,8 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+
+defined('MOODLE_INTERNAL') || die();
+
+$observers = [
+    // Add observers here if the provider needs to react to core events.
+];
