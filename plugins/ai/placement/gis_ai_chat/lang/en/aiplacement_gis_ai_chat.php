44d68f78d41455c552f3c28a4158b6f3ee4aef9d
prosper-ador <prosnyengka@gmail.com>
2025-10-20 07:57:48 +0100

    feat(placement-lang): add English language strings for chat

---
+++ plugins/ai/placement/gis_ai_chat/lang/en/aiplacement_gis_ai_chat.php
@@ -0,0 +1,14 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+
+$string['pluginname'] = 'GIS AI Chat placement';
+
+// Capabilities.
+$string['gis_ai_chat:generate_text'] = 'Use GIS AI Chat to generate text';
+
+// Errors.
+$string['processingfailed'] = 'Unable to process your request at this time.';
+$string['feedback'] = 'Feedback';
+
+// Privacy.
+$string['privacy:metadata'] = 'The GIS AI Chat placement does not store any personal data.';
