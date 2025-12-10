0690733b5932c69f60243f9a20d7f67a2347f80c
Leghadjeu-Christian <legochristianarthur@gmail.com>
2025-12-10 08:53:54 +0100

    fix: align required Moodle version with theme_boost dependency

--- plugins/gis-theme/adorsys_theme_v1/version.php
+++ plugins/gis-theme/adorsys_theme_v1/version.php
@@ -25,7 +25,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_adorsys_theme_v1';
 $plugin->version   = 2025080400;
-$plugin->requires  = 2020110900;
+$plugin->requires  = 2024042200;
 $plugin->release   = 'v1.0';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->dependencies = [
